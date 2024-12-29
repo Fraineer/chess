094b469da64a8e4c06b6dc574d01a23b552e3822
Fraineer <66141119+Fraineer@users.noreply.github.com>
2024-12-29 15:56:10 -0300

    commit print board wrong

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-print("hello chess")
+# cria uma lista [0, 0, 0, 0, 0, 0, 0, 0]
+board = [0] * 8
+letras = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h"]
+a = 0
 
-print("Let's go")
+for i in range(len(board)):
+    board[i] = [0] * 8
+
+for row in board:
+    a += 1
+    for col in row:
+        print(col, end = " ")
+    print (f"| {a}")
+
+for i in range(len(board)):
+    print("-" , end = " ")
+print("")
+for i in range(len(board)):
+    print(letras[i] , end = " ")
