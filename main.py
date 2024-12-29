2cd7baa986a941d8674b64def2177cd7b7180890
Fraineer <66141119+Fraineer@users.noreply.github.com>
2024-12-29 16:07:54 -0300

    commit board and printboard

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 # cria uma lista [0, 0, 0, 0, 0, 0, 0, 0]
 board = [0] * 8
-letras = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h"]
-a = 0
-
 for i in range(len(board)):
     board[i] = [0] * 8
 
-for row in board:
-    a += 1
-    for col in row:
-        print(col, end = " ")
-    print (f"| {a}")
+def printboard():
+    letras = ["a", "b", "c", "d", "e", "f", "g", "h"]
+    a = 0
+    for row in board:
+        a += 1
+        for col in row:
+            print(col, end = " ")
+        print (f"| {a}")
 
-for i in range(len(board)):
-    print("-" , end = " ")
-print("")
-for i in range(len(board)):
-    print(letras[i] , end = " ")
+    for i in range(len(board)):
+        print("-" , end = " ")
+    print("")
+    for i in range(len(board)):
+        print(letras[i] , end = " ")
+
+printboard()
