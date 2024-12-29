3f37f639bdfa9aea9f037f724a812d0128410f8a
Fraineer <66141119+Fraineer@users.noreply.github.com>
2024-12-29 19:14:46 -0300

    commit put_pieces

--- main.py
+++ main.py
@@ -4,19 +4,14 @@ for i in range(len(board)):
     board[i] = [0] * 8
 
 def printboard():
-    letras = ["a", "b", "c", "d", "e", "f", "g", "h"]
-    a = 0
     for row in board:
-        a += 1
         for col in row:
             print(col, end = " ")
-        print (f"| {a}")
+        print ("")
 
     for i in range(len(board)):
-        print("-" , end = " ")
+        print("" , end = " ")
     print("")
-    for i in range(len(board)):
-        print(letras[i] , end = " ")
 
 white_pieces_map =  {
     "wP": [(6,0),(6,1),(6,2),(6,3),(6,4),(6,5),(6,6),(6,7)],
@@ -28,10 +23,28 @@ white_pieces_map =  {
 }
 
 black_pieces_map =  {
-    "wP": [(1,0),(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7)],
-    "wN": [(0,1),(0,6)],
-    "wB": [(0,2),(0,5)],
-    "wR": [(0,0),(0,7)],
-    "wQ": [(0,3)],
-    "wK": [(0,4)]
+    "bP": [(1,0),(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7)],
+    "bN": [(0,1),(0,6)],
+    "bB": [(0,2),(0,5)],
+    "bR": [(0,0),(0,7)],
+    "bQ": [(0,3)],
+    "bK": [(0,4)]
 }
+
+def put_pieces(board):
+    #white pieces
+    for piece,squares in white_pieces_map.items():
+        for square in squares:
+            x,y = square[0] , square[1]
+            board[x][y] = piece
+
+
+    for piece, squares in black_pieces_map.items():
+        for square in squares:
+            x, y = square[0], square[1]
+            board[x][y] = piece
+
+
+
+put_pieces(board)
+printboard()
